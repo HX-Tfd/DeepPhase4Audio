49248b2841cd4f799fed7632223bb55005e9b820
HX-Tfd <oxotoxigon@gmail.com>
2024-12-03 13:59:55 +0100

    conditionally activate env and install deps

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 # create an venv and install dependencies
+. /etc/profile.d/modules.sh
+
+# Check if the virtual environment already exists
 # the cluster default is Python 3.12.3
-python3 -m venv .env
-source .env/bin/activate
-pip install -r requirements.txt
+if [ ! -d ".env" ]; then
+    echo "Creating Python virtual environment..."
+    python3 -m venv .env
+    source .env/bin/activate
+    echo "Installing dependencies..."
+    pip install -r requirements.txt
+else
+    echo "Virtual environment already exists. Activating..."
+    source .env/bin/activate
+fi
 
 #setup modules
-. /etc/profile.d/modules.sh
+echo "Loading modules..."
+
 module add gcc/11
 module add cuda/12.1
 
